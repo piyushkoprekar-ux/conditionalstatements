e2e72732274c734b329ab310e65b0291f662b034
piyush <piyushkoprekar@gmail.com>
2025-10-10 15:39:30 +0530

    new file for operators

---
+++ Aroperator.js
@@ -0,0 +1,22 @@
+//Arthimetic Operators
+let a = 5;
+let b = 2;
+
+console.log("a = ", a, "& b = " , b);
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+console.log("a % b = ", a % b);
+console.log("a ** b = ", a ** b);
+
+
+//Unary Operator
+//let a = 6;
+//let b = 3;
+ 
+//console.log("a = ", a, " & b = ", b);
+
+console.log("a++ = ", a++);//6
+console.log("a = ", a);//7
+
