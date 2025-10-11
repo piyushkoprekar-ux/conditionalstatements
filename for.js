9aa80ae5d46b563cb371f0775cf0233166872e88
piyush <piyushkoprekar@gmail.com>
2025-10-11 12:12:34 +0530

    For loop file

---
+++ for.js
@@ -0,0 +1,11 @@
+//Print 1 to 5
+for (let count = 1 ; count <= 5 ; count ++){
+    console.log("Hello World");
+}
+
+//Table loop
+const table = 8
+for (let i = 1; i <= 10; i++){
+    const result = table * i;
+    console.log("8 x",i, " = ", result);
+}
